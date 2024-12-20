df3bf19bbfcc46d534fc84cc7a17a71b1f4b566d
Artem Rudenko <artem@ottofeller.com>
2024-12-20 16:58:24 +0100

    Clean up example
    Leave one for endpoint and another one for worker

--- example/src/some.rs
+++ example/src/some.rs
@@ -1,8 +1,8 @@
 use lambda_http::{Body, Error, Request, RequestExt, Response};
-use skymacro::endpoint;
+use skymacro::{endpoint, worker};
 
 #[endpoint(name="Some", url_path="/some")]
-pub async fn some(event: Request) -> Result<Response<Body>, Error> {
+pub async fn some_endpoint(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
     let who = event
         .query_string_parameters_ref()
@@ -21,49 +21,8 @@ pub async fn some(event: Request) -> Result<Response<Body>, Error> {
     Ok(resp)
 }
 
-#[endpoint(url_path="/some1")]
-pub async fn some1(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
-
-#[endpoint(name="SomeTwo", url_path="/some2")]
-pub async fn some2(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
-        .map_err(Box::new)?;
-
-    Ok(resp)
-}
-
-#[endpoint(name="SomeThree", url_path="/some3")]
-pub async fn some3(event: Request) -> Result<Response<Body>, Error> {
+#[worker(name="aworker", queue="aqueue", concurrency=3)]
+pub async fn some_worker(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
     let who = event
         .query_string_parameters_ref()
