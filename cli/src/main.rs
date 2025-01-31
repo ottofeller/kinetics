b4b07ca3fb485616a5ecb7441a252f4b049a4203
Artem Rudenko <artem@ottofeller.com>
2025-01-31 17:00:25 +0100

    Move the deploy command code to its own file

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,11 @@
 mod crat;
+mod deploy;
 mod function;
 mod secret;
-use backend::deploy::{self, BodyCrate};
 use clap::{Parser, Subcommand};
 use crat::Crate;
 use eyre::{Ok, WrapErr};
 use function::Function;
-use secret::Secret;
 use std::path::{Path, PathBuf};
 static API_BASE: &str = "https://backend.usekinetics.com";
 
@@ -74,88 +73,6 @@ fn build() -> eyre::Result<()> {
     Ok(())
 }
 
-/// Bundle assets and upload to S3, assuming all functions are built
-fn bundle(functions: &Vec<Function>) -> eyre::Result<()> {
-    for function in functions {
-        function.bundle()?;
-    }
-
-    Ok(())
-}
-
-/// All bundled assets to S3
-async fn upload(functions: &Vec<Function>) -> eyre::Result<()> {
-    for function in functions {
-        #[derive(serde::Deserialize)]
-        struct PresignedUrl {
-            url: String,
-        }
-
-        let path = function.bundle_path();
-        let key = path.file_name().unwrap().to_str().unwrap();
-        println!("Uploading {path:?}...");
-        let client = reqwest::Client::new();
-
-        let presigned = client
-            .post(api_url("/upload"))
-            .json(&serde_json::json!({ "key": key }))
-            .send()
-            .await?
-            .json::<PresignedUrl>()
-            .await?;
-
-        client
-            .put(&presigned.url)
-            .body(tokio::fs::read(&path).await?)
-            .send()
-            .await?
-            .error_for_status()?;
-    }
-
-    Ok(())
-}
-
-/// Build and deploy all assets using CFN template
-async fn deploy() -> eyre::Result<()> {
-    let crat = crat().unwrap();
-    let functions = functions().wrap_err("Failed to bundle assets")?;
-    let client = reqwest::Client::new();
-    println!("Deploying \"{}\"...", crat.name);
-    bundle(&functions)?;
-    upload(&functions).await?;
-
-    client
-        .post(api_url("/deploy"))
-        .json(&serde_json::json!(deploy::JsonBody {
-            crat: BodyCrate {
-                toml: crat.toml_string.clone(),
-            },
-            functions: functions
-                .iter()
-                .map(|f| {
-                    deploy::BodyFunction {
-                        name: f.name().unwrap().to_string(),
-                        s3key: f.bundle_name(),
-                        toml: f.toml_string().unwrap(),
-                    }
-                })
-                .collect(),
-            secrets: vec![],
-        }))
-        .send()
-        .await
-        .wrap_err("Deployment request failed")?;
-
-    let secrets = Secret::from_dotenv(&crat.name)?;
-
-    for secret in secrets.iter() {
-        secret.sync().await?;
-    }
-
-    println!("Done!");
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() {
     let cli = Cli::parse();
@@ -173,7 +90,7 @@ async fn main() {
                 return;
             }
 
-            if let Err(error) = deploy().await {
+            if let Err(error) = deploy::deploy().await {
                 println!("{error:?}");
                 return;
             }
