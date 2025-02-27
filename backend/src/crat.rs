030f95f6ed9d04c08aa794e80570abfd1d65549a
flyin <dev@polesov.com>
2025-02-27 13:09:56 +0300

    Make resource fn public(crate)

--- backend/src/crat.rs
+++ backend/src/crat.rs
@@ -41,7 +41,7 @@ impl Crate {
     }
 
     /// The hash with all the resources specific to the function
-    fn resources(cargo_toml: &toml::Value) -> eyre::Result<Vec<crate::Resource>> {
+    pub(crate) fn resources(cargo_toml: &toml::Value) -> eyre::Result<Vec<crate::Resource>> {
         let mut result = vec![];
 
         for category_name in ["kvdb", "queue"] {
