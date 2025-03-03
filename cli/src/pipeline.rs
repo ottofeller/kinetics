b2c26c3159bde4b27fec13a34b4f1f2d82f4ed55
Gennadiy Gashev <gennadiy.gashev@ottofeller.com>
2025-03-03 17:51:12 +0100

    Add progress tracking and detailed logging to pipeline
    Introduced a progress tracking system using `indicatif` for building and deploying functions in parallel. Added real-time status updates, including stages like building, bundling, and uploading, with support for error handling and elapsed time reporting. This improves user visibility and debugging during pipeline executions.

--- cli/src/pipeline.rs
+++ cli/src/pipeline.rs
@@ -3,8 +3,11 @@ use crate::crat::Crate;
 use crate::function::Function;
 use eyre::{eyre, Context, Report};
 use futures::future;
-use std::sync::Arc;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
 use tokio::sync::Semaphore;
+use tokio::time::Duration;
 
 #[derive(Debug, Clone)]
 pub struct Pipeline {
@@ -20,15 +23,17 @@ impl Pipeline {
     }
 
     pub async fn run(self, functions: Vec<Function>) -> eyre::Result<()> {
-        // Define maximum number of parallel builds
+        let start_time = Instant::now();
         let semaphore = Arc::new(Semaphore::new(self.max_concurrent));
+        let pipeline_progress = PipelineProgress::new(functions.len() as u64);
 
-        println!("Building \"{}\"...", &self.crat.name);
         let client = Client::new(&self.is_directly).wrap_err("Failed to create client")?;
 
         let handles = functions.into_iter().map(|mut function| {
             let client = client.clone();
             let sem = Arc::clone(&semaphore);
+
+            let pipeline_progress = pipeline_progress.clone();
 
             tokio::spawn(async move {
                 // Acquire permit before sending request.
@@ -36,20 +41,38 @@ impl Pipeline {
 
                 let function_name = function.name()?;
 
-                function
-                    .build()
-                    .await
-                    .wrap_err(format!("Failed to build: {}", function_name))?;
+                let function_progress = pipeline_progress.new_progress(&function_name);
+                function_progress.log_stage("Building ");
+
+                function.build().await.map_err(|e| {
+                    function_progress.error();
+                    e.wrap_err(eyre!("Failed to build function: \"{}\"", function_name))
+                })?;
 
                 if !self.is_deploy_enabled {
+                    function_progress.done();
+                    pipeline_progress.increase_current_function_position();
                     return Ok(function);
                 }
 
-                function.bundle().await?;
+                function_progress.log_stage("Bundling ");
+
+                function.bundle().await.map_err(|e| {
+                    function_progress.error();
+                    e.wrap_err(eyre!("Failed to bundle function: \"{}\"", function_name))
+                })?;
+
+                function_progress.log_stage("Uploading");
 
                 crate::deploy::upload(&client, &mut function, &self.is_directly)
                     .await
-                    .wrap_err(format!("Failed to upload: {}", function.name()?))?;
+                    .map_err(|e| {
+                        function_progress.error();
+                        e.wrap_err(eyre!("Failed to upload function: \"{}\"", function_name))
+                    })?;
+
+                function_progress.done();
+                pipeline_progress.increase_current_function_position();
 
                 Ok::<Function, Report>(function)
             })
@@ -68,27 +91,46 @@ impl Pipeline {
             results.into_iter().partition(Result::is_ok);
 
         if !errors.is_empty() {
-            println!("Failed to build and upload functions:");
-            for error in errors {
-                println!("{:?}", error);
-            }
-            return Err(eyre!("Failed to build and upload functions"));
+            return Err(eyre!(
+                "Failed to process function(s): {:?}",
+                errors
+                    .into_iter()
+                    .map(Result::unwrap_err)
+                    .collect::<Vec<_>>()
+            ));
         }
 
-        println!("Build completed: \"{}\"", self.crat.name);
+        pipeline_progress.total_progress_bar.finish_and_clear();
 
         if !self.is_deploy_enabled {
+            println!(
+                "{} \"{}\" building and uploading in {:.2}s",
+                console::style("Finished").green().bold(),
+                self.crat.name,
+                start_time.elapsed().as_secs_f64(),
+            );
+
             return Ok(());
         }
 
-        println!("Deploying \"{}\"...", self.crat.name);
+        let deploying_progress = pipeline_progress.new_progress(&self.crat.name);
 
-        // It's safe to unwrap here because the errors have already been caught
+        deploying_progress.log_stage("Deploying");
+
         let functions: Vec<_> = ok_results.drain(..).map(Result::unwrap).collect();
 
         crate::deploy::deploy(&self.crat, &functions, &self.is_directly)
             .await
-            .wrap_err("Failed to deploy functions")?;
+            .wrap_err(eyre!("Failed to deploy functions"))?;
+
+        deploying_progress.progress_bar.finish_and_clear();
+
+        println!(
+            "{} \"{}\" has been successfully deployed in {:.2}s",
+            console::style("Finished").green().bold(),
+            self.crat.name,
+            start_time.elapsed().as_secs_f64(),
+        );
 
         Ok(())
     }
@@ -134,5 +176,110 @@ impl PipelineBuilder {
     pub fn set_max_concurrent(mut self, max_concurrent: usize) -> Self {
         self.max_concurrent = Some(max_concurrent);
         self
+    }
+}
+
+#[derive(Clone)]
+struct PipelineProgress {
+    multi_progress: MultiProgress,
+    total_progress_bar: ProgressBar,
+    total_functions: u64,
+    completed_functions_count: Arc<Mutex<u64>>,
+}
+
+impl PipelineProgress {
+    fn new(total_functions: u64) -> Self {
+        let multi_progress = MultiProgress::new();
+        let completed_functions_count = Arc::new(Mutex::new(0));
+        let total_progress_bar = multi_progress.add(ProgressBar::new(total_functions));
+
+        total_progress_bar.set_style(
+            ProgressStyle::default_bar()
+                .template("[{pos}/{len}] functions uploaded [{bar:40}] {percent}%")
+                .unwrap()
+                .progress_chars("=> "),
+        );
+
+        total_progress_bar.set_position(0);
+
+        Self {
+            multi_progress,
+            total_progress_bar,
+            total_functions,
+            completed_functions_count,
+        }
+    }
+
+    fn increase_current_function_position(&self) {
+        let mut count = self.completed_functions_count.lock().unwrap();
+        *count += 1;
+        self.total_progress_bar.set_position(*count as u64);
+
+        /* Workaround to make sure the progress bar finishes */
+        if *count as usize == (self.total_functions as usize) {
+            self.total_progress_bar.finish_with_message("");
+        }
+    }
+
+    fn new_progress(&self, resource_name: &str) -> Progress {
+        Progress::new(
+            &self.multi_progress,
+            &self.total_progress_bar,
+            resource_name,
+        )
+    }
+}
+
+struct Progress {
+    progress_bar: ProgressBar,
+    resource_name: String,
+}
+
+impl Progress {
+    fn new(
+        multi_progress: &MultiProgress,
+        total_progress_bar: &ProgressBar,
+        function_name: &str,
+    ) -> Self {
+        let function_progress_bar =
+            multi_progress.insert_before(&total_progress_bar, ProgressBar::new_spinner());
+
+        function_progress_bar.set_style(
+            ProgressStyle::default_spinner()
+                .template("{spinner:.green} {msg}")
+                .unwrap()
+                .tick_chars("⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏ "),
+        );
+
+        function_progress_bar.enable_steady_tick(Duration::from_millis(50));
+
+        Self {
+            progress_bar: function_progress_bar,
+            resource_name: function_name.to_string(),
+        }
+    }
+
+    fn log_stage(&self, stage: &str) {
+        self.progress_bar.set_message(format!(
+            "{} {}",
+            console::style(stage).green().bold(),
+            self.resource_name,
+        ));
+    }
+
+    fn done(&self) {
+        self.progress_bar.finish_with_message(format!(
+            "{}      {}",
+            console::style("Done").green().bold(),
+            self.resource_name,
+        ));
+    }
+
+    fn error(&self) {
+        self.progress_bar.finish_with_message(format!(
+            "{}     {}",
+            console::style("Error").red().bold(),
+            self.resource_name,
+        ));
     }
 }
