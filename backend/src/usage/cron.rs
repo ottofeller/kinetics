cc696183d3449d57f1310b5e9fe87eebf86d12ea
Artem Rudenko <artem@ottofeller.com>
2025-03-05 17:41:10 +0100

    Count invoications for each user

---
+++ backend/src/usage/cron.rs
@@ -0,0 +1,65 @@
+use crate::{env::env, user::UserBuilder};
+use aws_config::BehaviorVersion;
+use aws_sdk_dynamodb::types::AttributeValue::S;
+use aws_sdk_dynamodb::Client;
+use eyre::Context;
+use kinetics_macro::cron;
+use std::collections::HashMap;
+
+// Permissions:
+// {
+//     "Action": [
+//         "logs:CreateLogGroup",
+//         "logs:CreateLogStream",
+//         "logs:PutLogEvents",
+//         "cloudwatch:GetMetricStatistics",
+//         "tag:GetResources"
+//     ],
+//     "Resource": [
+//         "*",
+//     ],
+//     "Effect": "Allow"
+// }
+
+/// Bill users for usage
+///
+/// Also block free users if they go over free tier.
+#[cron(schedule = "rate(1 minute)", environment = {"TABLE_NAME": "kinetics"})]
+pub async fn cron(_secrets: &HashMap<String, String>) -> Result<(), Box<dyn std::error::Error>> {
+    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let db_client = Client::new(&config);
+    let table = env("TABLE_NAME")?;
+
+    let request = db_client
+        .scan()
+        .table_name(&table)
+        .filter_expression("begins_with(id, :prefix)")
+        .expression_attribute_values(":prefix", S("email#".to_string()));
+
+    let builder = UserBuilder::new(&db_client, &table);
+
+    for item in request.send().await?.items().iter() {
+        let email = item
+            .get("id")
+            .unwrap()
+            .as_s()
+            .unwrap()
+            .to_string()
+            .replace("email#", "");
+
+        let user = builder
+            .by_email(&email)
+            .await
+            .wrap_err("Failed to get user by email")?;
+
+        println!(
+            "Number of invocations for {} in this month: {:?}",
+            user.email,
+            user.invocations("month")
+                .await
+                .wrap_err("Failed to count invoications for user")?
+        );
+    }
+
+    Ok(())
+}
--- backend/src/user.rs
+++ backend/src/user.rs
@@ -1,5 +1,9 @@
+use aws_config::BehaviorVersion;
+use aws_sdk_cloudwatch::types::{Dimension, Statistic};
 use aws_sdk_dynamodb::types::AttributeValue::S;
-use eyre::Ok;
+use aws_sdk_s3::primitives::{DateTime, DateTimeFormat::DateTimeWithOffset};
+use chrono::{Datelike, Timelike, Utc};
+use eyre::{eyre, Context, Ok};
 use std::collections::HashMap;
 
 pub struct UserBuilder {
@@ -57,17 +61,157 @@ impl UserBuilder {
                 .send()
                 .await?;
 
-            return Ok(User { email, id: user_id });
+            return Ok(User {
+                email: email.to_string().clone(),
+                id: user_id,
+            });
         }
 
         return Ok(User {
-            email,
+            email: email.to_string().clone(),
             id: item.unwrap()["id"].as_s().unwrap().to_string(),
         });
+    }
+
+    /// Find existing user by email
+    ///
+    /// Return an error if no user exists with such email
+    pub async fn by_email(&self, email: &str) -> eyre::Result<User> {
+        let item = self
+            .db_client
+            .get_item()
+            .table_name(&self.db_table)
+            .key("id", S(format!("email#{}", email)))
+            .send()
+            .await?
+            .item;
+
+        if item.is_none() {
+            return Err(eyre!("User with email {} not found", email));
+        }
+
+        Ok(User {
+            email: email.to_string().clone(),
+            id: item.unwrap()["id"].as_s().unwrap().to_string(),
+        })
     }
 }
 
 pub struct User {
     pub email: String,
     pub id: String,
+}
+
+impl User {
+    /// The list of names of lambda functions deployed for the user
+    async fn functions(&self) -> eyre::Result<Vec<String>> {
+        let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+        let client = aws_sdk_resourcegroupstagging::Client::new(&config);
+        let mut list = vec![];
+        let mut has_more = true;
+        let mut token = String::default();
+
+        while has_more {
+            let mut res = client
+                .get_resources()
+                .tag_filters(
+                    aws_sdk_resourcegroupstagging::types::TagFilter::builder()
+                        .key("KINETICS_USERNAME")
+                        .values(self.email.clone())
+                        .build(),
+                )
+                .resource_type_filters("lambda:function");
+
+            if !token.is_empty() {
+                res = res.set_pagination_token(Some(token.clone()));
+            }
+
+            let response = res.send().await?;
+            has_more = !response
+                .clone()
+                .pagination_token()
+                .unwrap_or_default()
+                .is_empty();
+
+            if response.pagination_token().is_some() {
+                token = response.clone().pagination_token.unwrap();
+            }
+
+            list.extend(
+                response
+                    .resource_tag_mapping_list()
+                    .iter()
+                    .filter_map(|res| {
+                        // There is seemingly no other way to get function name
+                        res.resource_arn().map(|arn| {
+                            arn.to_string()
+                                .split(":")
+                                .last()
+                                .unwrap_or_default()
+                                .to_string()
+                        })
+                    }),
+            );
+        }
+
+        Ok(list)
+    }
+
+    /// Total number of unvocations for user's functioncs
+    pub async fn invocations(&self, period: &str) -> eyre::Result<u16> {
+        let mut total = 0;
+        let now_raw = Utc::now();
+        let end_time = DateTime::from_str(&now_raw.to_rfc3339(), DateTimeWithOffset)?;
+
+        let start_time = match period {
+            "month" => DateTime::from_str(
+                &now_raw
+                    .with_day(1)
+                    .unwrap()
+                    .with_hour(0)
+                    .unwrap()
+                    .with_minute(0)
+                    .unwrap()
+                    .with_second(0)
+                    .unwrap()
+                    .to_rfc3339(),
+                DateTimeWithOffset,
+            )
+            .unwrap(),
+            _ => eyre::bail!("Invalid period"),
+        };
+
+        let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+        let cloudwatch_client = aws_sdk_cloudwatch::Client::new(&config);
+        let functions = self.functions().await?;
+
+        for function in functions {
+            let metrics = cloudwatch_client
+                .get_metric_statistics()
+                .namespace("AWS/Lambda")
+                .metric_name("Invocations")
+                .dimensions(
+                    Dimension::builder()
+                        .name("FunctionName")
+                        .value(function.clone())
+                        .build(),
+                )
+                .start_time(start_time)
+                .end_time(end_time)
+                .period(30 * 24 * 60 * 60) // 30 days in seconds
+                .statistics(Statistic::Sum)
+                .send()
+                .await
+                .wrap_err("Failed to get CloudWatch metrics")?;
+
+            total += metrics
+                .datapoints()
+                .iter()
+                .map(|dp| dp.sum().unwrap_or(0.0) as u16)
+                .sum::<u16>();
+
+        }
+
+        Ok(total)
+    }
 }
