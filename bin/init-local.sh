13b7086e01e29700dda624e4896754d49ba979bb
flyin <dev@polesov.com>
2025-03-13 16:12:24 +0300

    Add exports and fix double quotes issues

--- bin/init-local.sh
+++ bin/init-local.sh
@@ -3,16 +3,16 @@ echo "Initializing environment variables..."
 
 if [ ! -f "backend/local.env" ]; then
     touch backend/local.env
-    echo "KINETICS_USE_PRODUCTION_DOMAIN=false" >> backend/local.env
+    echo "export KINETICS_USE_PRODUCTION_DOMAIN=false" >> backend/local.env
 
     read -r -p "Enter your email: " kinetics_username
-    echo "KINETICS_USERNAME=\"$kinetics_username\"" >> backend/local.env
+    echo "export KINETICS_USERNAME=$kinetics_username" >> backend/local.env
 
     # Replace @ to AT and . to DOT in $kinetics_username
     kinetics_username_escaped=${kinetics_username//@/AT}
     kinetics_username_escaped=${kinetics_username_escaped//./DOT}
 
-    echo "KINETICS_USERNAME_ESCAPED=\"$kinetics_username_escaped\"" >> backend/local.env
+    echo "export KINETICS_USERNAME_ESCAPED=$kinetics_username_escaped" >> backend/local.env
 fi
 
 # Get KMS key
@@ -25,7 +25,7 @@ if ! grep -q "KINETICS_KMS_KEY_ID" backend/local.env; then
       exit 1
   fi
 
-  echo "KINETICS_KMS_KEY_ID=$KMS_KEY_ID" >> backend/local.env
+  echo "export KINETICS_KMS_KEY_ID=$KMS_KEY_ID" >> backend/local.env
 fi
 
 # Create a new S3 bucket
@@ -33,7 +33,7 @@ if ! grep -q "KINETICS_S3_BUCKET_NAME" backend/local.env; then
   BUCKET_NAME="kinetics-builds-$(date +%s)-$(openssl rand -hex 4)"
   echo "Creating S3 bucket: $BUCKET_NAME..."
   aws s3 mb "s3://$BUCKET_NAME"
-  echo "KINETICS_S3_BUCKET_NAME=$BUCKET_NAME" >> backend/local.env
+  echo "export KINETICS_S3_BUCKET_NAME=$BUCKET_NAME" >> backend/local.env
 fi
 
 # Get the latest Cloudfront domain name
@@ -61,7 +61,7 @@ if ! grep -q "KINETICS_API_BASE" backend/local.env; then
   # Return back to the project root
   cd .. || exit
 
-  echo "KINETICS_API_BASE=https://$CLOUDFRONT_DOMAIN" >> backend/local.env
+  echo "export KINETICS_API_BASE=https://$CLOUDFRONT_DOMAIN" >> backend/local.env
 fi
 
 source backend/local.env
