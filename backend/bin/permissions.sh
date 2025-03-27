9823b012f1237264de78c693a8d47a06e8c68c35
Roman Vasilev <90195036+quesabe@users.noreply.github.com>
2025-03-27 12:53:51 +0300

    Pull role logical ID with cloudformation describe-stack-resource

--- backend/bin/permissions.sh
+++ backend/bin/permissions.sh
@@ -4,13 +4,21 @@ kinetics_username_escaped="${email//@/AT}"
 kinetics_username_escaped="${kinetics_username_escaped//./DOT}"
 
 dir=$(dirname "$0")
+crate_name=backend
+stack_name="$kinetics_username_escaped-$crate_name"
 
 aws iam put-role-policy \
-    --role-name "EndpointRole${kinetics_username_escaped}DbackendDDeployDeploy" \
+    --role-name $(aws cloudformation describe-stack-resource \
+        --stack-name $stack_name \
+        --logical-resource-id "EndpointRole${kinetics_username_escaped}DbackendDDeployDeploy" | \
+        jq -r .StackResourceDetail.PhysicalResourceId) \
     --policy-name DeployResourcesPolicy \
     --policy-document file://$dir/deploy-policy.json
 
 aws iam put-role-policy \
-    --role-name EndpointRoleDbackendDUploadUpload \
+    --role-name $(aws cloudformation describe-stack-resource \
+        --stack-name $stack_name \
+        --logical-resource-id "EndpointRoleDbackendDUploadUpload" | \
+        jq -r .StackResourceDetail.PhysicalResourceId) \
     --policy-name UploadLoggingPolicy \
     --policy-document file://$dir/upload-policy.json
