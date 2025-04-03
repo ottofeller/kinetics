c4049cd87eae82f4c14ac288756bbc074a3dbdcd
Artem Rudenko <artem@ottofeller.com>
2025-04-03 17:46:54 +0200

    Return generic message in status endpoint response

--- backend/src/stack/status.rs
+++ backend/src/stack/status.rs
@@ -5,7 +5,7 @@ use crate::stack::Stack;
 use aws_config::BehaviorVersion;
 use aws_sdk_cloudformation::types::StackEvent;
 use aws_sdk_sqs::operation::send_message::builders::SendMessageFluentBuilder;
-use eyre::Result;
+use eyre::{ContextCompat, Result, WrapErr};
 use kinetics_macro::endpoint;
 use lambda_http::{Body, Error, Request, Response};
 use serde_json::json;
@@ -38,7 +38,7 @@ pub struct JsonBody {
 })]
 pub async fn status(
     event: Request,
-    _secrets: &HashMap<String, String>,
+    secrets: &HashMap<String, String>,
     _queues: &HashMap<String, SendMessageFluentBuilder>,
 ) -> Result<Response<Body>, Error> {
     fn map_stack_event(event: &StackEvent) -> serde_json::Value {
@@ -86,7 +86,7 @@ pub async fn status(
         for event in events {
             let is_stack_event = event
                 .resource_type()
-                .unwrap()
+                .wrap_err("Missing resource type")?
                 .eq("AWS::CloudFormation::Stack");
 
             let json = map_stack_event(&event);
@@ -102,7 +102,10 @@ pub async fn status(
 
             // Once failure or success event found no need in searching for other failure or success event
             if matches!(
-                event.resource_status().unwrap().as_str(),
+                event
+                    .resource_status()
+                    .wrap_err("Missing resource status")?
+                    .as_str(),
                 "UPDATE_ROLLBACK_COMPLETE"
                     | "UPDATE_ROLLBACK_FAILED"
                     | "CREATE_FAILED"
@@ -110,16 +113,21 @@ pub async fn status(
                     | "DELETE_FAILED"
             ) && end_event_failure.is_none()
                 && end_event_success.is_none()
+                && start_event.is_none()
                 && is_stack_event
             {
                 end_event_failure = Some(json.clone());
             }
 
             if matches!(
-                event.resource_status().unwrap().as_str(),
+                event
+                    .resource_status()
+                    .wrap_err("Missing resource status")?
+                    .as_str(),
                 "UPDATE_COMPLETE" | "CREATE_COMPLETE" | "DELETE_COMPLETE"
             ) && end_event_success.is_none()
                 && end_event_failure.is_none()
+                && start_event.is_none()
                 && is_stack_event
             {
                 end_event_success = Some(json.clone());
@@ -153,7 +161,33 @@ pub async fn status(
             if status.contains("FAILED") {
                 if let Some(resource_type) = event.get("ResourceType").and_then(|t| t.as_str()) {
                     if resource_type != "AWS::CloudFormation::Stack" {
-                        errors.push(event.get("Reason"));
+                        let message = reqwest::Client::new()
+                            .post("https://api.openai.com/v1/responses")
+                            .header("Content-Type", "application/json")
+                            .header(
+                                "Authorization",
+                                format!("Bearer {}", secrets.get("OPENAI_API_KEY").wrap_err("OPENAI_API_KEY secret is missing")?),
+                            )
+                            .json(&serde_json::json!({
+                                "model": "gpt-4o",
+                                "input": format!("Replace specific error message with a more general one. E.g. replace this \"Object {{\"Reason\": String(\"kinetics already exists in stack arn:aws:cloudformation:us-east-1:430118855033:stack/artemATottofellerDOTcom-backend/e6f64650-ff54-11ef-ad34-12e618ec57ab\"), \"ResourceType\": String(\"AWS::DynamoDB::Table\"), \"Status\": String(\"CREATE_FAILED\")\" with \"Database name \"kinetics\" is not unique\". Return only the error message string, don't add anything else.
+                                Here is the error message — {}", event.to_string())
+                            }))
+                            .send()
+                            .await
+                            .wrap_err("Request failed")?
+                            .json::<serde_json::Value>()
+                            .await
+                            .wrap_err("Failed to parse response to JSON")?;
+
+                        errors.push(
+                            message["output"][0]["content"][0]["text"]
+                                .as_str()
+                                .unwrap()
+                                .trim_matches('"')
+                                .to_owned()
+                                .to_string(),
+                        );
                     }
                 }
             }
