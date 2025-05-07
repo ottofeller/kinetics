f9180ff4c04397545bd08febb2b359be03930687
Roman Vasilev <90195036+quesabe@users.noreply.github.com>
2025-05-07 15:19:04 +0300

    Add rustdocs for each macro

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -2,6 +2,13 @@ use kinetics_parser::{Cron, Endpoint, Worker};
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
 
+/// Lambda endpoint
+///
+/// Parameters:
+/// - `name`: override the function name
+/// - `url_path`: URL path of the endpoint
+/// - `environment`: environment variables
+/// - `queues`: SQS queues accessible from the lambda
 #[proc_macro_attribute]
 pub fn endpoint(attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the macro attributes in order to validate the inputs,
@@ -10,6 +17,12 @@ pub fn endpoint(attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
 
+/// Cron lambda
+///
+/// Parameters:
+/// - `name`: override the function name
+/// - `schedule`: [Schedule expression](https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-resource-scheduler-schedule.html#cfn-scheduler-schedule-scheduleexpression)
+/// - `environment`: environment variables
 #[proc_macro_attribute]
 pub fn cron(attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the macro attributes in order to validate the inputs,
@@ -18,6 +31,14 @@ pub fn cron(attr: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
 
+/// Worker lambda
+///
+/// Parameters:
+/// - `name`: override the function name
+/// - `queue_alias`: alias of the queue processed by the worker
+/// - `concurrency`: max number of concurrent workers
+/// - `fifo`: set to true to enable FIFO processing
+/// - `environment`: environment variables
 #[proc_macro_attribute]
 pub fn worker(attr: TokenStream, item: TokenStream) -> TokenStream {
     // Parse the macro attributes in order to validate the inputs,
