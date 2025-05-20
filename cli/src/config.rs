2273a5d8d0c7c80968fded5ad7a2cd76bae5419b
Roman Vasilev <90195036+quesabe@users.noreply.github.com>
2025-05-20 14:24:49 +0300

    Remove once_cell_try feature and move to stable channel

--- cli/src/config.rs
+++ cli/src/config.rs
@@ -12,21 +12,18 @@ pub(crate) struct Config<'a> {
 static CONFIG: OnceLock<Config> = OnceLock::new();
 
 pub(crate) fn build_config() -> Result<&'static Config<'static>, Error> {
-    CONFIG.get_or_try_init(|| {
+    let home_dir = std::env::var("HOME").map_err(|_| {
+        log::error!("Failed to get $HOME");
+
+        Error::new(
+            "$HOME is missing",
+            Some("Your shell might not be supported."),
+        )
+    })?;
+
+    Ok(CONFIG.get_or_init(|| {
         let api_base =
             option_env!("KINETICS_API_BASE").unwrap_or("https://backend.usekinetics.com/");
-
-        let home_dir = match std::env::var("HOME") {
-            Ok(home_dir) => home_dir,
-            Err(_) => {
-                log::error!("Failed to get $HOME");
-
-                return Err(Error::new(
-                    "$HOME is missing",
-                    Some("Your shell might not be supported."),
-                ));
-            }
-        };
 
         let build_path_raw = Path::new(&home_dir).join(".kinetics");
 
@@ -47,12 +44,12 @@ pub(crate) fn build_config() -> Result<&'static Config<'static>, Error> {
                 .into_boxed_str(),
         );
 
-        Ok(Config {
+        Config {
             api_base,
             build_path,
             credentials_path,
-        })
-    })
+        }
+    }))
 }
 
 pub fn api_url(path: &str) -> String {
