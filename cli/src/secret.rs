741e76749e82e4c804d520b32a88e07e09723866
Artem Rudenko <artem@ottofeller.com>
2025-05-21 11:17:43 +0200

    Suppress the message about missing .env.secrets
    It is not that important

--- cli/src/secret.rs
+++ cli/src/secret.rs
@@ -8,6 +8,13 @@ pub struct Secret {
 impl Secret {
     /// Read secrets from the .env file
     pub fn from_dotenv() -> Vec<Self> {
+        // Return early if the .env.secrets file doesn't exist,
+        // to avoid rust_dotenv throwing error message
+        if !std::path::Path::new(".env.secrets").exists() {
+            log::warn!("No .env.secrets file found");
+            return vec![];
+        }
+
         let mut result = vec![];
         let dotenv = DotEnv::new("secrets");
 
