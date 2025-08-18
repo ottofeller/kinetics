5e4f343e08694d0f1d241dbe5433151345f7f0b8
Artem Rudenko <artem@ottofeller.com>
2025-08-18 11:42:36 +0200

    Apply internal retries struct in example

--- examples/src/basic/worker.rs
+++ examples/src/basic/worker.rs
@@ -1,5 +1,5 @@
-use aws_lambda_events::sqs::{BatchItemFailure, SqsBatchResponse, SqsEvent};
-use kinetics::tools::queue::Client as QueueClient;
+use aws_lambda_events::sqs::SqsEvent;
+use kinetics::tools::queue::{Client as QueueClient, Retries as QueueRetries};
 use kinetics_macro::worker;
 use lambda_runtime::{Error, LambdaEvent};
 use std::collections::HashMap;
@@ -14,11 +14,9 @@ pub async fn worker(
     event: LambdaEvent<SqsEvent>,
     _secrets: &HashMap<String, String>,
     _queues: &HashMap<String, QueueClient>,
-) -> Result<SqsBatchResponse, Error> {
-    let mut sqs_batch_response = SqsBatchResponse::default();
+) -> Result<QueueRetries, Error> {
+    let mut retries = QueueRetries::new();
 
-    // Always return the first record from the input batch in batch item failure, just for example
-    // Doing so will force the worker to process the item again on the next iteration
     let record = match event.payload.records.first() {
         Some(record) => record,
         None => {
@@ -32,11 +30,9 @@ pub async fn worker(
     let body = serde_json::Value::from(record.body.clone().unwrap());
     println!("Got body: {body:?}");
 
-    sqs_batch_response
-        .batch_item_failures
-        .push(BatchItemFailure {
-            item_identifier: record.message_id.clone().unwrap_or_default(),
-        });
+    // This always returns the first record from the input batch in retries, just for example
+    // Doing so will force the worker to process the item again on the next iteration
+    retries.add(&record.message_id.clone().unwrap_or_default());
 
-    Ok(sqs_batch_response)
+    Ok(retries)
 }
