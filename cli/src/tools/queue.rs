6b1f37eff14f93bab7e6463650fae1beebcea6af
Artem Rudenko <artem@ottofeller.com>
2025-08-18 11:42:10 +0200

    Implement internal retries struct

--- cli/src/tools/queue.rs
+++ cli/src/tools/queue.rs
@@ -1,4 +1,5 @@
 use aws_sdk_sqs::operation::send_message::builders::SendMessageFluentBuilder;
+use aws_lambda_events::sqs::{BatchItemFailure, SqsBatchResponse};
 
 pub struct Client {
     queue: SendMessageFluentBuilder,
@@ -21,5 +22,40 @@ impl Client {
     ) -> eyre::Result<()> {
         self.queue.clone().message_body(message).send().await?;
         Ok(())
+    }
+}
+
+/// Items to be retried by worker queue
+///
+/// Worker function must return a Retries struct with ids of items that need
+/// to be retried.
+#[derive(Default)]
+pub struct Retries {
+    ids: Vec<String>,
+}
+
+impl Retries {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn add(&mut self, item: &str) {
+        self.ids.push(item.to_string());
+    }
+
+    /// Serialize to the format which can be understood by queue API
+    pub fn collect(&self) -> SqsBatchResponse {
+        let mut sqs_batch_response = SqsBatchResponse::default();
+
+        for id in self.ids.iter() {
+            sqs_batch_response
+                .batch_item_failures
+                .push(BatchItemFailure {
+                    item_identifier: id.clone(),
+                });
+
+        }
+
+        sqs_batch_response
     }
 }
