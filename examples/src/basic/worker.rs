714e1de25e1d382540e085fd0d5550956a5d44a2
Artem Rudenko <artem@ottofeller.com>
2025-08-21 14:15:10 +0200

    Delete queue alias from examples and amend docs

--- examples/src/basic/worker.rs
+++ examples/src/basic/worker.rs
@@ -8,7 +8,7 @@ use std::collections::HashMap;
 /// Always returns the first record as failed to process. It will then be retried.
 /// Test locally with the following command:
 /// kinetics invoke BasicWorkerWorker --payload '{"name": "John"}'
-#[worker(fifo = true, queue_alias = "example")]
+#[worker(fifo = true)]
 pub async fn worker(
     records: Vec<QueueRecord>,
     _secrets: &HashMap<String, String>,
