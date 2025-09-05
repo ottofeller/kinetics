8886acd5e75e5ce883f0e7a6be0bacc80b741904
Artem Rudenko <artem@ottofeller.com>
2025-09-05 18:20:34 +0200

    Delete obsolete function

--- cli/src/utils.rs
+++ cli/src/utils.rs
@@ -1,12 +1,3 @@
-/// Replace any unwanted character in resource name
-/// with its uppercase-alpha counterpart
-pub fn escape_resource_name(name: &str) -> String {
-    name.replace("@", "AT")
-        .replace(".", "DOT")
-        .replace("-", "HYPHEN")
-        .replace("_", "UNDRSC")
-}
-
 /// Unique resource name
 ///
 /// Construct a readable name by escaping non-ascii chars, and appending a hash of
