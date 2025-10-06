c9047bddd714a31b9559fbf332f0cefafdab7417
Artem Rudenko <artem@ottofeller.com>
2025-10-06 16:09:34 +0200

    Implement "envs list" command

---
+++ cli/src/commands/envs.rs
@@ -0,0 +1,58 @@
+use crate::build::prepare_crates;
+use crate::config::build_config;
+use crate::crat::Crate;
+use crossterm::style::Stylize;
+use eyre::{eyre, Context};
+use kinetics_parser::Parser;
+use std::path::PathBuf;
+
+/// Lists all environment variables for all functions in the current crate
+pub async fn list(crat: &Crate) -> eyre::Result<()> {
+    let functions = prepare_crates(PathBuf::from(build_config()?.kinetics_path), &crat, &vec![])?;
+    let parsed_functions = Parser::new(Some(&crat.path))?.functions;
+
+    if functions.is_empty() {
+        println!("{}", "No functions found".yellow());
+        return Ok(());
+    }
+
+    for function in functions {
+        let path = parsed_functions
+            .iter()
+            .find(|f| {
+                f.func_name(false)
+                    .inspect_err(|e| log::error!("Failed to get function name: {e:?}"))
+                    .unwrap()
+                    == function.name
+            })
+            .ok_or(eyre!("Parsed artifact has no funciton name"))
+            .inspect_err(|e| log::error!("{e:?}"))?
+            .relative_path
+            .to_owned();
+
+        let function_name = function.clone().name;
+
+        // Get environment variables for this function
+        let env_vars = function
+            .environment()
+            .wrap_err(format!("Failed to get envs for {function_name}"))?;
+
+        if env_vars.is_empty() {
+            continue;
+        }
+
+        println!(
+            "{} {}",
+            function_name.bold(),
+            format!("from {}", path).dim()
+        );
+
+        for (key, value) in env_vars {
+            println!("{} {}", key.dim(), value.black());
+        }
+
+        println!();
+    }
+
+    Ok(())
+}
