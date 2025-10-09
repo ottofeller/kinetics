f4c13f9144bf215fa1cc19aaa5102aecb0b16979
Artem Rudenko <artem@ottofeller.com>
2025-10-09 16:31:04 +0200

    Change the presentation of the list of projects
    Follow the common style guide

--- cli/src/commands/projects.rs
+++ cli/src/commands/projects.rs
@@ -1,47 +1,17 @@
-use crate::crat::Crate;
 use crate::error::Error;
 use crate::project::Project;
-use eyre::ContextCompat;
-use futures::stream::{self, StreamExt};
-use tabled::settings::{peaker::Priority, style::Style, Settings, Width};
-use tabled::{Table, Tabled};
-use terminal_size::{terminal_size, Width as TerminalWidth};
+use color_eyre::owo_colors::OwoColorize;
+use crossterm::style::Stylize;
+use eyre::Context;
 
 /// Prints out the list of all projects
 pub async fn list() -> Result<(), Error> {
-    #[derive(Tabled, Clone)]
-    struct ProjectRow {
-        #[tabled(rename = "Name")]
-        name: String,
-        #[tabled(rename = "Status")]
-        status: String,
-    }
-
-    let projects = stream::iter(Project::all().await?)
-        .map(async |project| {
-            Crate::status_by_name(&project.name)
-                .await
-                .map(|status| ProjectRow {
-                    name: project.name,
-                    status: status.status,
-                })
-        })
-        .buffer_unordered(3) // Run up to 3 requests concurrently.
-        .collect::<Vec<_>>()
+    Project::all()
         .await
-        .into_iter()
-        .collect::<Result<Vec<_>, _>>()?;
-
-    // Check terminal size and setup output table accordingly
-    let (TerminalWidth(width), _) = terminal_size().wrap_err("failed to obtain a terminal size")?;
-    let width: usize = width.into();
-    let settings = Settings::default()
-        .with(Width::wrap(width).priority(Priority::max(true)))
-        .with(Width::increase(width));
-
-    let mut table = Table::new(projects);
-    table.with(Style::modern()).with(settings);
-    println!("Projects\n{}", table);
+        .inspect_err(|e| log::error!("Failed to load list of projects: {e}"))
+        .wrap_err("Request failed. Tray again later.")?
+        .iter()
+        .for_each(|p| println!("{}\n{}\n\n", p.name.clone().bold(), p.url.dimmed()));
 
     Ok(())
 }
