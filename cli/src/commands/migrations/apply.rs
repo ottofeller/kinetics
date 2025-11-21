c14465d7353a64c8b333c800d28990fd500457cf
flyin <dev@polesov.com>
2025-11-21 22:14:20 +0300

    Apply migrations using SQL database

--- cli/src/commands/migrations/apply.rs
+++ cli/src/commands/migrations/apply.rs
@@ -1,13 +1,30 @@
+// FIXME Move from the commands namespace
+use crate::commands::invoke::{docker::Docker, service::LocalSqlDB};
+use crate::config::build_config;
 use crate::migration::Migration;
 use eyre::Context;
+use std::path::PathBuf;
 
+/// Apply migrations to the local SQL DB
 pub async fn apply(path: Option<&str>) -> eyre::Result<()> {
     let current_dir = std::env::current_dir().wrap_err("Failed to get current dir")?;
+
+    // Run local SQL DB
+    let mut docker = Docker::new(&PathBuf::from(&build_config()?.kinetics_path));
+    let sqldb = LocalSqlDB::new();
+    let db_connection_string = sqldb.connection_string();
+
+    // Run docker containers for SQL DB
+    docker.with_sqldb(sqldb);
+    docker.start()?;
+
+    // After provisioning is up and running, we can apply migrations
+    docker.provision().await?;
 
     Migration::new(current_dir.join(path.unwrap_or("migrations")).as_path())
         .await
         .wrap_err("Failed to initialize migrations")?
-        .apply()
+        .apply(db_connection_string)
         .await
         .wrap_err("Failed to apply migrations")?;
 
--- cli/src/migration.rs
+++ cli/src/migration.rs
@@ -1,5 +1,6 @@
 use color_eyre::owo_colors::OwoColorize;
 use eyre::Context;
+use sqlx::{Pool, Postgres, Row};
 use std::path::Path;
 
 pub struct Migration<'a> {
@@ -15,7 +16,73 @@ impl<'a> Migration<'a> {
         Ok(Self { path })
     }
 
-    pub async fn apply(&self) -> eyre::Result<()> {
+    pub async fn apply(&self, connection_string: String) -> eyre::Result<()> {
+        println!("{}", console::style("Applying migrations...").dimmed());
+
+        let all_migrations = self.migrations().await?;
+
+        let connection = sqlx::PgPool::connect(&connection_string)
+            .await
+            .wrap_err("Failed to connect to database")?;
+
+        self.ensure_migrations_table(&connection)
+            .await
+            .wrap_err("Failed to fetch migrations table")?;
+
+        // Get latest applied migration
+        let result = sqlx::query("SELECT MAX(id) FROM schema_migrations")
+            .fetch_one(&connection)
+            .await?;
+
+        let last_db_id: String = result
+            .try_get::<Option<String>, _>(0)
+            .unwrap_or_default()
+            .unwrap_or("0".to_string());
+
+        // Get latest migration file name
+        let last_file_id = all_migrations.last().cloned().unwrap_or("0".to_string());
+
+        // Check if there are migrations to apply
+        if last_db_id >= last_file_id || all_migrations.is_empty() {
+            println!(
+                "{}",
+                console::style("No migrations to apply...").red().bold()
+            );
+            return Ok(());
+        }
+
+        let mut tx = connection.begin().await?;
+
+        for migration in all_migrations {
+            if migration <= last_db_id {
+                continue;
+            }
+
+            println!(
+                "{}: {}",
+                console::style("Applying migration").green(),
+                console::style(&migration).dimmed()
+            );
+
+            let sql = tokio::fs::read_to_string(&self.path.join(&migration))
+                .await
+                .wrap_err("Failed to read migration file")?;
+
+            sqlx::query(&sql)
+                .execute(&mut *tx)
+                .await
+                .wrap_err("Failed to apply migration")?;
+
+            sqlx::query("INSERT INTO schema_migrations (id) VALUES ($1)")
+                .bind(&migration)
+                .execute(&mut *tx)
+                .await?;
+        }
+
+        println!("{}", console::style("All migrations were applied").green());
+
+        // Commit transaction
+        tx.commit().await?;
         Ok(())
     }
 
@@ -27,12 +94,13 @@ impl<'a> Migration<'a> {
             .replace(" ", "_")
             .chars()
             .filter(|c| c.is_alphanumeric() || *c == '_')
+            .take(100)
             .collect::<String>();
 
         let filepath = self.path.join(format!("{}_{}.up.sql", timestamp, name));
 
         // TODO Add some helpful comments to the migration file
-        tokio::fs::write(self.path.join(&filepath), "")
+        tokio::fs::write(&filepath, "")
             .await
             .wrap_err("Failed to create migration file")?;
 
@@ -55,6 +123,50 @@ impl<'a> Migration<'a> {
             ))
             .dimmed(),
         );
+
+        Ok(())
+    }
+
+    /// Retrieves the list of migration files from the specified directory.
+    ///
+    /// It returns only file names that end with `.up.sql`.
+    /// Files ordered by names in ASC (oldest first) order
+    async fn migrations(&self) -> eyre::Result<Vec<String>> {
+        let mut read_dir = tokio::fs::read_dir(self.path).await?;
+        let mut entries = Vec::new();
+
+        while let Some(entry) = read_dir.next_entry().await? {
+            let path = entry.path();
+
+            if !path.is_file() {
+                continue;
+            }
+
+            if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
+                if name.ends_with(".up.sql") {
+                    entries.push(name.to_string());
+                }
+            }
+        }
+
+        // Sort files by name (oldest first)
+        entries.sort();
+        Ok(entries)
+    }
+
+    /// Ensures that the migrations table exists in the database.
+    async fn ensure_migrations_table(&self, connection: &Pool<Postgres>) -> eyre::Result<()> {
+        sqlx::query(
+            r#"
+            CREATE TABLE IF NOT EXISTS schema_migrations (
+                id VARCHAR(255) PRIMARY KEY,
+                applied_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            );
+            "#,
+        )
+        .execute(connection)
+        .await
+        .wrap_err("Failed to create migrations table")?;
 
         Ok(())
     }
