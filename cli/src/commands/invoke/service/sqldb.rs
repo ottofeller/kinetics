dc52a677d4d88614d68fa55aee38459c874b5c5b
flyin <dev@polesov.com>
2025-11-26 17:00:53 +0300

    Improve SQLDB cleanup logic by dropping all tables in the current schema.

--- cli/src/commands/invoke/service/sqldb.rs
+++ cli/src/commands/invoke/service/sqldb.rs
@@ -102,10 +102,13 @@ impl LocalSqlDB {
     async fn clean_database(&self, connection: &Pool<Postgres>) -> eyre::Result<()> {
         sqlx::raw_sql(
             r#"
-            DROP SCHEMA public CASCADE;
-            CREATE SCHEMA public;
-            GRANT ALL ON SCHEMA public TO postgres;
-            GRANT ALL ON SCHEMA public TO public;
+            DO $$ DECLARE
+                r RECORD;
+            BEGIN
+                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
+                    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
+                END LOOP;
+            END $$;
         "#,
         )
         .execute(connection)
