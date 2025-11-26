e1c726d3eecfa8b061d74c1c4e752d53a99005ef
flyin <dev@polesov.com>
2025-11-26 17:17:38 +0300

    Revert "Improve SQLDB cleanup logic by dropping all tables in the current schema."
    This reverts commit dc52a677d4d88614d68fa55aee38459c874b5c5b.

--- cli/src/commands/invoke/service/sqldb.rs
+++ cli/src/commands/invoke/service/sqldb.rs
@@ -102,13 +102,10 @@ impl LocalSqlDB {
     async fn clean_database(&self, connection: &Pool<Postgres>) -> eyre::Result<()> {
         sqlx::raw_sql(
             r#"
-            DO $$ DECLARE
-                r RECORD;
-            BEGIN
-                FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = current_schema()) LOOP
-                    EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-                END LOOP;
-            END $$;
+            DROP SCHEMA public CASCADE;
+            CREATE SCHEMA public;
+            GRANT ALL ON SCHEMA public TO postgres;
+            GRANT ALL ON SCHEMA public TO public;
         "#,
         )
         .execute(connection)
