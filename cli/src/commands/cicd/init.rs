38c5403bcb347109154e9b7110f229702e4dfd81
Artem Rudenko <artem@ottofeller.com>
2025-11-27 16:34:48 +0100

    Make input param optional

--- cli/src/commands/cicd/init.rs
+++ cli/src/commands/cicd/init.rs
@@ -1,12 +1,14 @@
 use crate::{commands::cicd::github, project::Project};
 
-/// Initialize a deployment workflow within an existing kinetics project.
-pub async fn init(project: &Project, github: bool) -> eyre::Result<()> {
+/// Initialize a deployment workflow within an existing kinetics project
+///
+/// Currently only supports GitHub, but expected to expand in the future.
+pub async fn init(project: &Project, _github: bool) -> eyre::Result<()> {
     println!(
         "{}",
         console::style("Adding GitHub workflow...").yellow()
     );
-    
+
     github::workflow(project)?;
     println!("{}", console::style("Done").bold().green());
     Ok(())
