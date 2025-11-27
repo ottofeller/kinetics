3112f718a5e1c8be4d32ecd8a5becd4f0cca2770
Artem Rudenko <artem@ottofeller.com>
2025-11-27 16:29:47 +0100

    Delete redundant condition

--- cli/src/commands/cicd/init.rs
+++ cli/src/commands/cicd/init.rs
@@ -2,15 +2,12 @@ use crate::{commands::cicd::github, project::Project};
 
 /// Initialize a deployment workflow within an existing kinetics project.
 pub async fn init(project: &Project, github: bool) -> eyre::Result<()> {
-    if !github {
-        println!(
-            "{}",
-            console::style("No CI/CD provider selected. Use github.").yellow()
-        );
-    }
-
+    println!(
+        "{}",
+        console::style("Adding GitHub workflow...").yellow()
+    );
+    
     github::workflow(project)?;
-
     println!("{}", console::style("Done").bold().green());
     Ok(())
 }
