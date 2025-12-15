f6331d494a54c3ba853686017e69594d1e5a327a
flyin <dev@polesov.com>
2025-12-15 18:05:57 +0300

    Add rustdoc

--- cli/src/api/db/connect.rs
+++ cli/src/api/db/connect.rs
@@ -1,10 +1,12 @@
 use serde::{Deserialize, Serialize};
 
+/// Response from /stack/db/connect
 #[derive(Deserialize, Serialize)]
 pub struct Response {
     pub connection_string: String,
 }
 
+/// Request body for /stack/db/connect
 #[derive(Serialize, Deserialize)]
 pub struct Request {
     pub project: String,
