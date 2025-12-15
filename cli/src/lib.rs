0b2df55f4d35666a464f7b2bab0eb167303eb8e7
Roman Vasilev <90195036+quesabe@users.noreply.github.com>
2025-12-15 21:36:35 +0300

    Add explanation for pub modifier in lib.rs
    Some modules are inherently public, some expose their functionality but can be turned private in future, and the remaining ones just provide types used by pub functions.

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -1,16 +1,26 @@
+// Public structs for kinetics API.
 pub mod api;
+// Export of cli:run for calls with custom config.
 pub mod cli;
+// Export Client type for use with other public interfaces.
 pub mod client;
 mod commands;
+// Export deploy config for use as custom config along with cli::run.
 pub mod config;
 mod credentials;
+// Export cli error type for use cli::run.
 pub mod error;
+// Export Function type for use with cli::run struct.
 pub mod function;
 mod logger;
+// Reexport macros from this crate.
 pub mod macros;
 mod migrations;
 mod process;
+// Export Project type for use with cli::run struct.
 pub mod project;
 mod secrets;
+// Export SqlDb type for use with tools::config.
 pub mod sqldb;
+// Export public tools.
 pub mod tools;
