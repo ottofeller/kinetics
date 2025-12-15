b188edf58c228057d241fe6bd7676c1bf66786e2
flyin <dev@polesov.com>
2025-12-15 19:02:24 +0300

    Fix urls in rustdoc

--- cli/src/api/project/sqldb.rs
+++ cli/src/api/project/sqldb.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-/// Response from /stack/db/connect
+/// Response from /stack/sqldb/connect
 #[derive(Deserialize, Serialize)]
 pub struct Response {
     pub connection_string: String,
 }
 
-/// Request body for /stack/db/connect
+/// Request body for /stack/sqldb/connect
 #[derive(Serialize, Deserialize)]
 pub struct Request {
     pub project: String,
