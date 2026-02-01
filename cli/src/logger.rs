461654e79075a0f04ddaf714d11dd06893ba01f3
Roman Vasilev <90195036+quesabe@users.noreply.github.com>
2026-02-01 22:38:52 +0300

    Use indicatif-log-bridge
    The bridge helps avoid interference between progress bars and logs.

--- cli/src/logger.rs
+++ cli/src/logger.rs
@@ -1,16 +1,38 @@
+use std::sync::OnceLock;
+
+use indicatif::MultiProgress;
+use indicatif_log_bridge::LogWrapper;
+
 /// Set up log levels, formatting, and other configurations for the logger
-pub struct Logger;
+pub struct Logger {
+    multi_progress: MultiProgress,
+}
 
-impl Logger {
-    pub fn init() -> Self {
-        let mut builder = env_logger::Builder::new();
+static LOGGER: OnceLock<Logger> = OnceLock::new();
 
-        // No logs shown by default, only human-friendlt messages
-        // Enable logs output with "export RUST_LOG=error" in terminal
-        builder.filter_level(log::LevelFilter::Off);
+impl<'a> Logger {
+    pub fn init() -> &'a Self {
+        LOGGER.get_or_init(|| {
+            let logger = env_logger::Builder::from_env(
+                // No logs shown by default, only human-friendly messages
+                // Enable logs output with "export RUST_LOG=error" in terminal
+                env_logger::Env::default().default_filter_or("off"),
+            )
+            .build();
 
-        builder.parse_default_env();
-        builder.init();
-        Logger
+            let level = logger.filter();
+            let multi_progress = MultiProgress::new();
+
+            LogWrapper::new(multi_progress.clone(), logger)
+                .try_init()
+                .unwrap();
+            log::set_max_level(level);
+
+            Self { multi_progress }
+        })
+    }
+
+    pub fn multi_progress() -> &'a MultiProgress {
+        &Self::init().multi_progress
     }
 }
