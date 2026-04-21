a635f1123372b91dad4f0d94beadad97438db4c0
Artem Rudenko <artem@ottofeller.com>
2026-04-21 11:52:19 +0200

    Add email validator in orgs validators

--- cli/src/api/orgs/validators.rs
+++ cli/src/api/orgs/validators.rs
@@ -4,6 +4,13 @@ use regex::Regex;
 static NAME_REGEX: Lazy<Regex> =
     Lazy::new(|| Regex::new(r"^[a-zA-Z\-_]{2,32}$").expect("Failed to init regexp"));
 
+static EMAIL_REGEX: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
+    )
+    .expect("Failed to init regexp")
+});
+
 pub struct Name;
 
 impl Name {
@@ -13,5 +20,17 @@ impl Name {
 
     pub fn message() -> String {
         "Invalid \"name\". Must be 2-32 characters long and contain only letters (a-z, A-Z), hyphens (-), and underscores (_).".into()
+    }
+}
+
+pub struct Email;
+
+impl Email {
+    pub fn validate(email: &str) -> bool {
+        EMAIL_REGEX.is_match(email)
+    }
+
+    pub fn message() -> String {
+        "Invalid \"email\". Must be a valid email address.".into()
     }
 }
