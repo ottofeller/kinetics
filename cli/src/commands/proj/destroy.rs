1ef1b12c06868f71906d60fa58adca58971a287a
flyin <dev@polesov.com>
2026-06-09 16:39:07 +0300

    Clear project cache after deploy and destroy

--- cli/src/commands/proj/destroy.rs
+++ cli/src/commands/proj/destroy.rs
@@ -102,6 +102,14 @@ impl Runner for DestroyRunner<'_> {
             console::style("Project destroyed").green()
         ))?;
 
+        Project::clear_cache().map_err(|e| {
+            self.error(
+                Some("Failed to clear project cache"),
+                Some(&e.to_string()),
+                Some(e.into()),
+            )
+        })?;
+
         self.writer.json(json!({"success": true}))?;
         Ok(())
     }
