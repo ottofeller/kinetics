9787d98e478b54054f025ea649860c38a977fc22
flyin <dev@polesov.com>
2026-06-16 16:51:35 +0300

    Use Project Name for Workspace Packages

--- cli/src/project.rs
+++ cli/src/project.rs
@@ -64,7 +64,7 @@ pub struct Observability {
 
 impl Project {
     fn new(path: PathBuf, name: String) -> Self {
-        let workspace = Workspace::from_path(&path).ok().unwrap_or_default();
+        let workspace = Workspace::from_path(&path, &name).ok().unwrap_or_default();
 
         Self {
             path,
