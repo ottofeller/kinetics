0a12b74ba899e85acc0aeaff0b0f79340b984774
flyin <dev@polesov.com>
2026-07-31 23:23:45 +0300

    Copy extensionless files unchanged

--- cli/src/project/parse.rs
+++ cli/src/project/parse.rs
@@ -564,7 +564,7 @@ impl Project {
     ) -> eyre::Result<()> {
         let dst_path_full = dst_dir.join(dst_rel_path);
         // For all non .rs files just copy it.
-        if src.extension().is_some_and(|ext| ext != "rs") {
+        if src.extension().is_none_or(|ext| ext != "rs") {
             log::debug!("Copy without checksum {dst_path_full:?}");
             return fs::copy(src, &dst_path_full)
                 .wrap_err_with(|| format!("Failed to copy file {src:?} -> {dst_path_full:?}"))
