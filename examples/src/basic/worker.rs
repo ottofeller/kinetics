49bb0196c71feefc9640167e44f2b4ae6d05e304
flyin <dev@polesov.com>
2026-08-13 18:17:23 +0300

    Support payload files and batched worker payloads

--- examples/src/basic/worker.rs
+++ examples/src/basic/worker.rs
@@ -8,10 +8,11 @@ use tower::BoxError;
 
 /// A queue worker
 ///
-/// Always returns the first record as failed to process. It will then be retried.
+/// Processes and prints every record in the input batch.
+/// Always returns only the first record as failed to process. It will then be retried.
 ///
 /// Test locally with the following command:
-/// kinetics invoke BasicWorkerWorker --payload '{"name": "John"}'
+/// kinetics invoke BasicWorkerWorker --payload '[{"name": "John"}, {"name": "Jane"}]'
 #[worker(fifo = true, batch_size = 2)]
 pub async fn worker(
     records: Vec<QueueRecord>,
@@ -20,7 +21,7 @@ pub async fn worker(
 ) -> Result<QueueRetries, BoxError> {
     let mut retries = QueueRetries::new();
 
-    let record = match records.first() {
+    let first_record = match records.first() {
         Some(record) => record,
         None => {
             return Err(Box::new(std::io::Error::new(
@@ -30,12 +31,15 @@ pub async fn worker(
         }
     };
 
-    let body = serde_json::Value::from(record.body.clone().unwrap());
-    println!("Got body: {body:?}");
+    // Process every record in the input batch.
+    for record in &records {
+        let body = serde_json::Value::from(record.body.clone().unwrap());
+        println!("Got message: {body:?}");
+    }
 
-    // Optionally return the first record from the input batch in retries, just for example
+    // Optionally return only the first record from the input batch in retries, just for example
     // Doing so will force the worker to process the item again on the next iteration
-    retries.add(&record.message_id.clone().unwrap_or_default());
+    retries.add(&first_record.message_id.clone().unwrap_or_default());
 
     Ok(retries)
 }
